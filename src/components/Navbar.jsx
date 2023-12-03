16ece794e1cd5819c402ef78f7974049776d52e0
Yohann <73314529+yohann69@users.noreply.github.com>
2023-12-03 12:37:52 +0100

    Base project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import {styles} from "../styles.js";
+import {navLinks} from "../constants";
+import {logo, menu, close} from "../assets"
+
+const Navbar = () => {
+  return (
+    <nav
+    className={
+      `${styles.paddingX} w-full flex`
+    }>
+
+    </nav>
+  )
+}
+
+export default Navbar
